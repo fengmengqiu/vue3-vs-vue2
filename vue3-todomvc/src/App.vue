c72a0d17ae0ee98f50cb9de20ac894b0fd500730
fengmengqiu <cnbaifm@163.com>
2020-08-03 21:54:24 +0800

    💬 change text

--- vue3-todomvc/src/App.vue
+++ vue3-todomvc/src/App.vue
@@ -74,11 +74,11 @@ export default {
       todos: [
         { id: "1", title: "吃饭", completed: false },
         { id: "2", title: "睡觉", completed: false },
-        { id: "3", title: "学习vue3", completed: false },
-        { id: "4", title: "写文章", completed: false },
+        { id: "3", title: "看书", completed: false },
+        { id: "4", title: "玩游戏", completed: false },
         { id: "5", title: "看动画", completed: false },
-        { id: "6", title: "逛知乎", completed: false },
-        { id: "7", title: "撸狗", completed: false },
+        { id: "6", title: "逛淘宝", completed: false },
+        { id: "7", title: "看NBA", completed: false },
         { id: "8", title: "摸鱼", completed: true },
       ],
     });
