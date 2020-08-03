1a3e3f654e560ed07b3820a1ec77d069f9420b54
fengmengqiu <cnbaifm@163.com>
2020-08-03 21:24:57 +0800

    🐛 add function removeTodo

--- vue3-todomvc/src/App.vue
+++ vue3-todomvc/src/App.vue
@@ -1,50 +1,76 @@
 <template>
-<section class="todoapp">
-  <header class="header" :class="{fixed:top>130}">
-
-    <h1>{{x}},{{y}}</h1>
-    <input class="new-todo"
-      placeholder="想干的事"
-      v-model="newTodo"
-      @keyup.enter="addTodo">
-  </header>
-  <section class="main" v-show="todos.length">
-    <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allDone">
-    <label for="toggle-all">Mark all as complete</label>
-    <ul class="todo-list">
-      <li v-for="todo in todos"
-        class="todo"
-        :key="todo.id"
-        :class="{ completed: todo.completed }"
+  <section class="todoapp">
+    <header
+      class="header"
+      :class="{fixed:top>130}"
+    >
+      <h1>{{x}},{{y}}</h1>
+      <input
+        class="new-todo"
+        placeholder="想干的事"
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+      />
+    </header>
+    <section
+      class="main"
+      v-show="todos.length"
+    >
+      <input
+        id="toggle-all"
+        class="toggle-all"
+        type="checkbox"
+        v-model="allDone"
+      />
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        <li
+          v-for="todo in todos"
+          class="todo"
+          :key="todo.id"
+          :class="{ completed: todo.completed }"
         >
-        <div class="view">
-          <input class="toggle" type="checkbox" v-model="todo.completed" >
+          <div class="view">
+            <input
+              class="toggle"
+              type="checkbox"
+              v-model="todo.completed"
+            />
 
-          <label>{{ todo.title }}</label>
-          <button class="destroy" @click="removeTodo(todo)"></button>
-        </div>
-      </li>
-    </ul>
+            <label>{{ todo.title }}</label>
+            <button
+              class="destroy"
+              @click="removeTodo(todo)"
+            ></button>
+          </div>
+        </li>
+      </ul>
+    </section>
+    <footer
+      class="footer"
+      v-show="todos.length"
+      v-cloak
+    >
+      <span class="todo-count">
+        <strong>{{ remaining }}</strong> left
+      </span>
+      <button
+        class="clear-completed"
+        @click="removeCompleted"
+        v-show="todos.length > remaining"
+      >Clear completed</button>
+    </footer>
   </section>
-  <footer class="footer" v-show="todos.length" v-cloak>
-    <span class="todo-count">
-      <strong>{{ remaining }}</strong>  left
-    </span>
-    <button class="clear-completed" @click="removeCompleted" v-show="todos.length > remaining">
-      Clear completed
-    </button>
-  </footer>
-</section>
 </template>
 
 <script>
-import {ref,reactive,toRefs,computed} from 'vue'
-import useScroll from './scroll'
-import useMouse from './mouse'
+import { ref, reactive, toRefs, computed } from "vue";
+import useScroll from "./scroll";
+import useMouse from "./mouse";
 export default {
-  setup(){
+  setup() {
     const state = reactive({
-      newTodo:'',
+      newTodo: "",
       todos: [
         { id: "1", title: "吃饭", completed: false },
         { id: "2", title: "睡觉", completed: false },
@@ -55,56 +81,68 @@ export default {
         { id: "7", title: "撸狗", completed: false },
         { id: "8", title: "摸鱼", completed: true },
       ],
-    })
-    function addTodo(){
-      var value = state.newTodo && state.newTodo.trim()
+    });
+    function addTodo() {
+      var value = state.newTodo && state.newTodo.trim();
       if (!value) {
-        return
+        return;
       }
       state.todos.push({
         id: state.todos.length + 1,
         title: value,
-        completed: false
-      })
-      state.newTodo = ""
+        completed: false,
+      });
+      state.newTodo = "";
+    }
+
+    function removeTodo(todo) {
+      console.log(todo);
+      state.todos = state.todos.filter((i) => todo != i);
     }
 
     const remaining = computed(
-      () => state.todos.filter(todo => !todo.completed).length
+      () => state.todos.filter((todo) => !todo.completed).length
     );
     const allDone = computed({
-      get: function() {
+      get: function () {
         return remaining.value === 0;
       },
-      set: function(value) {
-        state.todos.forEach(function(todo) {
+      set: function (value) {
+        state.todos.forEach(function (todo) {
           todo.completed = value;
         });
-      }
+      },
     });
     function removeCompleted() {
-      state.todos = state.todos.filter(todo => !todo.completed);
+      state.todos = state.todos.filter((todo) => !todo.completed);
     }
-    const {top} = useScroll()
-    const {x,y} = useMouse()
+    const { top } = useScroll();
+    const { x, y } = useMouse();
     return {
-      ...toRefs(state),remaining,allDone,top,
-      x,y,top,
-      addTodo,removeCompleted
-    }
-  }
-}
+      ...toRefs(state),
+      remaining,
+      allDone,
+      top,
+      x,
+      y,
+      top,
+      addTodo,
+      removeTodo,
+      removeCompleted,
+    };
+  },
+};
 </script>
 
 
 <style>
-.header.fixed{
+.header.fixed {
   background: #fff;
   position: fixed;
-  top:0;
-  left:0;
-  right:0;
-  width:100%;
-  z-index:100;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  z-index: 100;
 }
 </style>
